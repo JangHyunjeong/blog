c4d1648b6fa8c894c0244c7b27299e4ec35f157c
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2025-03-12 00:03:08 +0900

    Style: 상세페이지 헤더 및 스타일 추가

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -5,6 +5,13 @@ const PostDetail = async ({ params: paramsPromise }: { params: Promise<{ slug: s
   const post = await getPostDetailBySlug(params.slug);
   return (
     <div>
+      <div className="mb-10 pb-7 border-gray-300 border-b ">
+        <span className="border border-gray-300 rounded-md px-3 py-1 text-sm text-gray-500">
+          {post.category}
+        </span>
+        <p className="font-bold mt-4 text-3xl">{post.title}</p>
+        <p className="text-sm text-gray-500 mt-3">{post.date}</p>
+      </div>
       <div className="prose prose-base">
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
       </div>
